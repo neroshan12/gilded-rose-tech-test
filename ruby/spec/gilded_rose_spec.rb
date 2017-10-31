fa6a7a84f7b89ffa49947b4a24dc87e2e1472460
Nero Siva <neroshan12@gmail.com>
2017-10-31 17:47:30 +0000

    Add test for negative quality

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,9 +1,7 @@
 require './lib/gilded_rose.rb'
 require './lib/item.rb'
 
-
 describe GildedRose do
-
   describe "#update_quality" do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
@@ -12,4 +10,9 @@ describe GildedRose do
     end
   end
 
+  it 'does not allow the quality of an item to be negative' do
+    items = [Item.new("foo", 0, 0)]
+    GildedRose.new(items).update_quality()
+    expect(items[0].quality).to eq 0
+  end
 end
